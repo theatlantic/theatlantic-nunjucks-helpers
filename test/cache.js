6a083dca1dce3157568b3a3d37397eb9f84c5af7
Joey Nichols <joeyquarters@gmail.com>
2018-01-11 16:51:12 -0500

    Set up tests with mocha/chai

---
+++ test/cache.js
@@ -0,0 +1,19 @@
+const nunjucks = require('nunjucks');
+const chai = require('chai');
+const extensions = require('../index');
+const CacheExtension = extensions.Cache;
+const expect = chai.expect;
+
+describe('#cache block tag', function() {
+  let env;
+
+  before(function() {
+    env = new nunjucks.Environment();
+    env.addExtension('cache', new CacheExtension());
+  });
+
+  it('should exist in the nunjucks environment', function() {
+    expect(env.hasExtension('cache')).to.be.true;
+  });
+
+});
---
+++ test/with.js
@@ -0,0 +1,20 @@
+const nunjucks = require('nunjucks');
+const chai = require('chai');
+const extensions = require('../index');
+
+const WithExtension = extensions.With;
+const expect = chai.expect;
+
+describe('#with block tag', function() {
+  let env;
+
+  before(function() {
+    env = new nunjucks.Environment();
+    env.addExtension('with', new WithExtension());
+  });
+
+  it('should exist in the nunjucks environment', function() {
+    expect(env.hasExtension('with')).to.be.true;
+  });
+
+});
