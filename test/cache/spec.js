95ee0f109ebbfc6af22b61ea27c5b89eae371ef4
Joey Nichols <joeyquarters@gmail.com>
2018-01-12 12:22:11 -0500

    Add hopeful tests for the future

--- test/cache/spec.js
+++ test/cache/spec.js
@@ -9,7 +9,8 @@ describe('#cache block tag', function() {
   let env;
 
   before(function() {
-    env = new nunjucks.Environment();
+    nunjucks.installJinjaCompat();
+    env = nunjucks.configure('test/cache');
     env.addExtension('cache', new CacheExtension());
   });
 
@@ -17,4 +18,19 @@ describe('#cache block tag', function() {
     expect(env.hasExtension('cache')).to.be.true;
   });
 
+  it('should not error when compiling', function() {
+    const tplString = '{% cache %}I required heavy DB operations.{% endcache %}';
+    const result = nunjucks.compile(tplString);
+    expect(result).to.be.an('object');
+  });
+
+  // This extension is currently just a no-op, so it doesn't actually render correctly!
+  // @todo Rewrite this extension so that if we actually want to render it via javascript,
+  //       it will work
+  //
+  // it('should render correctly', function() {
+  //   const result = nunjucks.render('templates/cache_block.html');
+  //   const expected = 'I required heavy DB operations.';
+  //   expect(result).to.equal(expected);
+  // });
 });
--- test/with/spec.js
+++ test/with/spec.js
@@ -9,7 +9,8 @@ describe('#with block tag', function() {
   let env;
 
   before(function() {
-    env = new nunjucks.Environment();
+    nunjucks.installJinjaCompat();
+    env = nunjucks.configure('test/with');
     env.addExtension('with', new WithExtension());
   });
 
@@ -17,4 +18,24 @@ describe('#with block tag', function() {
     expect(env.hasExtension('with')).to.be.true;
   });
 
+  it('should not error when compiling', function() {
+    const tplString = '{% with author = article.author %}{{ author }}{% endwith %}';
+    const result = nunjucks.compile(tplString);
+    expect(result).to.be.an('object');
+  });
+
+  // This extension is currently just a no-op, so it doesn't actually render correctly!
+  // @todo Rewrite this extension so that if we actually want to render it via javascript,
+  //       it will work
+  //
+  // it('should render correctly', function() {
+  //   const props = {
+  //     article: {
+  //       author: 'David Frum'
+  //     }
+  //   };
+  //   const result = nunjucks.render('templates/with_block.html', props);
+  //   const expected = 'David Frum';
+  //   expect(result).to.equal(expected);
+  // });
 });
